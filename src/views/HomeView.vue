e7d344069d80c4dbd2e53c9265526783bc684438
Wynter Stroman <Wynter.Stroman@bcbssc.com>
2026-06-02 13:28:59 -0400

    added prev. css styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts">
+<script setup>
+import  { ref } from 'vue'
+import DisplayDetails from '@/components/DisplayDetails.vue'
+import SummaryDisplay from '@/components/SummaryDisplay.vue'
+import ProjectForm from '@/components/ProjectForm.vue'
+import Search from '@/components/Search.vue'
+import StatusFiltering from '@/components/StatusFiltering.vue'
+
+const viewType = ref('all')
 
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+
 </template>
