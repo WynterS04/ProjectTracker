b38b7ec07926931c6cb162c56492eee182ffd5fb
Wynter Stroman <Wynter.Stroman@bcbssc.com>
2026-06-03 08:36:40 -0400

    Adding store for project form

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,5 @@ import { ref } from 'vue'
 </script>
 
 <template> <!--BCBS Intern Project Tracker-->
-  <header>
-    <h1>BCBS Intern Project Tracker</h1>
-  </header>
-
   <RouterView />
 </template>
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,6 +1,19 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+
+export type ProjectStatus = 'Not Started' | 'In Progress' | 'Complete'
+
+export interface Project { 
+id: number 
+name: string 
+owner: string 
+status: ProjectStatus 
+dueDate: string 
+description: string
+
+}
+
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
