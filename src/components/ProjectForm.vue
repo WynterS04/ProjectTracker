a806ea087d91dbc528d2813dcb71a92727f175f2
Wynter Stroman <Wynter.Stroman@bcbssc.com>
2026-06-03 09:14:31 -0400

    form edit

--- src/components/ProjectForm.vue
+++ src/components/ProjectForm.vue
@@ -6,24 +6,24 @@ import { ref } from 'vue'
 <template>
     <form>
         <label for="name">Project Name</label>
-        <input id="name">
+        <input id="name" required>
         
         <label for="owner">Owner</label>
-        <input id="owner">
+        <input id="owner" required>
 
         <label for="date">Due Date</label>
-        <input id="date" type="date">
+        <input id="date" type="date" required>
 
         <label for="status">Status</label>
-        <select id="status">
+        <select id="status" required>
             <option>Not Started</option>
             <option>In Progress</option>
             <option>Completed</option>
         </select>
 
         <label for="description">Description</label>
-        <textarea id="description"></textarea>
+        <textarea id="description" required></textarea>
 
         <input class="button" type="submit" value="Submit">
     </form>
-</template> <!--Is the ID mandatory in the form-->
+</template> <!--Is the ID mandatory in the form?-->
