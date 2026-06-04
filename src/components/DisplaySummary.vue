2041846741e63078a315169bf9a072352c9e7750
Wynter Stroman <Wynter.Stroman@bcbssc.com>
2026-06-04 14:51:30 -0400

    added icons

--- src/components/DisplaySummary.vue
+++ src/components/DisplaySummary.vue
@@ -4,11 +4,12 @@ import { useProjectStore } from '@/stores/ProjectStore';
 
 const store = useProjectStore()
 
+const totals = store.summmarizeProjects
 
 const  statuses = ref([
-    { status: 'Not Started', color: 'grey', total: 3},
-    { status: 'In Progress', color: 'orange', total: 4},
-    { status: 'Completed', color: 'green', total: 2}
+    { status: 'Not Started', color: 'grey', total:totals[1]},
+    { status: 'In Progress', color: 'orange', total:totals[2]},
+    { status: 'Completed', color: 'green', total:totals[3]}
 ])
 
 </script>
@@ -16,12 +17,9 @@ const  statuses = ref([
 <template>
     <div class="project-summary">
         <h2>Summary</h2>
-        <div 
-        v-for="status in statuses"
-        :key="status.id"
-        class="color-circle" 
-        :style="{ backgroundColor: status.color }" > 
+        <div v-for="status in statuses" :key="status.id"> 
             <p>{{ status.status }} - {{ status.total }}</p>
         </div>
+        <p>Total: {{ totals[0] }}</p>
     </div>
 </template>
--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useProjectStore } from '@/stores/ProjectStore'
-import type { Project } from '@/stores/ProjectStore';
+import type { Project } from '@/stores/ProjectStore'
+import ProjectForm from '@/components/ProjectForm.vue'
 
 const store = useProjectStore()
 
 const headers = ['Id', 'Name', 'Owner', 'Date', 'Status', 'Description']
+
+const confirmDelete = () => {
+
+}
 
 </script>
 
@@ -25,9 +30,23 @@ const headers = ['Id', 'Name', 'Owner', 'Date', 'Status', 'Description']
                     <td>{{ row.date }}</td>
                     <td>{{ row.status }}</td>
                     <td>{{ row.description }}</td>
+                    <i @click="confirmDelete" class="fas fa-trash fa-lg" id="trash"></i>
+                    <i class="fa-solid fa-pen-to-square fa-lg" id="edit"></i>
                 </tr>
             </tbody>
         </table>
   </div>
 
 </template>
+
+<style>
+#trash, #edit {
+    margin: 0px 8px 0px;
+}
+#trash:hover {
+    color: rgb(243, 44, 31);
+}
+#edit:hover {
+    color: rgba(14, 134, 231, 1.00)
+}
+</style>
