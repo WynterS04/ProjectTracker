e19e198f4d18c8fb8b7aea1f1d8f724decf6c6a7
Wynter Stroman <Wynter.Stroman@bcbssc.com>
2026-06-11 09:12:41 -0400

    fixing configs

--- tests/file.test.ts
+++ tests/file.test.ts
@@ -1,7 +1,10 @@
+/// <reference types="vitest/globals" />
+
 import { setActivePinia, createPinia } from 'pinia';
 import { describe, it, expect } from 'vitest'
 import { flushPromises, mount } from '@vue/test-utils';
 import HomeView from '@/views/HomeView.vue'
+import { useProjectStore } from '@/stores/ProjectStore'
 
 describe('HomeView', () => {
     beforeEach(() => {
@@ -17,12 +20,16 @@ describe('HomeView', () => {
     });
 
     it('display summary of projects', async () => {
+        const store = useProjectStore()
+        const totals = store.summmarizeProjects
         const wrapper = mount(HomeView);
-        await wrapper.find('[data-test="project-summary-button"]').trigger('click')
+        await wrapper.find('[data-test="project-summary-button"]').trigger('click');
 
-        expect(wrapper.text()).toContain('Not Started')
-        expect(wrapper.text()).toContain('In Progress')
-        expect(wrapper.text()).toContain('Complete')
+        expect(wrapper.text()).toContain('Not Started - ${totals[1]}')
+        expect(wrapper.text()).toContain('In Progress - ${totals[2]}')
+        expect(wrapper.text()).toContain('Complete - ${totals[3]}')
+        expect(wrapper.text()).toContain('Total')
+        expect(wrapper.text()).toContain(totals[0])
     }
 
     )
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,7 +15,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
